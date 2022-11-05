7f165189e93aaee8b350ff96fab2347600e1dada
NoePerezFab <noeperez@e-gisnet.com>
2022-11-05 14:42:47 -0600

    Se anadio el context para tomcat y la clase inicializadora

--- src/main/java/com/escom/tt/TtApplication.java
+++ src/main/java/com/escom/tt/TtApplication.java
@@ -2,6 +2,10 @@ package com.escom.tt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.ErrorPageFilter;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootApplication
 public class TtApplication {
@@ -10,4 +14,16 @@ public class TtApplication {
 		SpringApplication.run(TtApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder  configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+    
+    @Override
+    protected WebApplicationContext run(SpringApplication application) {
+        application.getSources().remove(ErrorPageFilter.class);
+        return super.run(application);
+    }
+    
+    private static Class<ErppApplication> applicationClass = TtApplication.class;
 }
