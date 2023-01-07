b173ee87c055896686c3ed4b502f0baa08df873a
NoePerezFab <noeperez@e-gisnet.com>
2023-01-07 01:39:21 -0600

    Se arreglaron la relaciones en bd

--- src/main/java/com/escom/tt/config/WebSocketConfig.java
+++ src/main/java/com/escom/tt/config/WebSocketConfig.java
@@ -12,11 +12,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
         registry.addEndpoint("/ws" ).setAllowedOriginPatterns("*").withSockJS();
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
         registry.setApplicationDestinationPrefixes("/app");
+        
         registry.enableSimpleBroker("/call");
     }
     
