6ccfc4b135e2f39f5e0a5cc41d3ed59c6e4ee53c
NoePerezFab <noeperez@e-gisnet.com>
2023-01-10 16:55:02 -0600

    .

--- src/main/java/com/escom/tt/repository/RutinaRepository.java
+++ src/main/java/com/escom/tt/repository/RutinaRepository.java
@@ -2,9 +2,10 @@
 package com.escom.tt.repository;
 
 import com.escom.tt.domain.Rutina;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface RutinaRepository extends JpaRepository<Rutina, Long>{
-    
+    public List<Rutina> findAllByUsuarioId(Long idUsuario);
 }
